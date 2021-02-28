59826ab78238d91a20facfb749d503124c3bf43a
krabs <>
2021-02-28 16:56:00 -0500

    No longer attempt to load the Stomp priority function

--- ZBa_BattleMod_CE/Lua/3-Functions/4-Physics/Collision/Lib_Priority_Index.lua
+++ ZBa_BattleMod_CE/Lua/3-Functions/4-Physics/Collision/Lib_Priority_Index.lua
@@ -1,4 +1,5 @@
 local B = CBW_Battle
+local PFunc = B.PriorityFunction
 
 local xyangle = B.GetCollideRelativeAngle
 local zangle = B.GetZCollideAngle
@@ -49,3 +50,33 @@ B.SPriority_CanDamage = function(mo,other)
 	if not (mo.player and P_PlayerCanDamage(mo.player,other)) then return false
 	else return true end
 end
+
+B.AddPriorityFunction = function(name,func)
+	if not(name) then //Invalid name
+		B.Warning("Invalid argument #1 provided for CBW_Battle.AddPriorityFunction()")
+	return nil end
+	if not(type(name) == "string") then //Name isn't a string
+		B.Warning("Argument #1 for CBW_Battle.AddPriorityFunction() must be a string!")
+	return nil end
+	if PFunc[name] != nil then //Name already exists
+		B.Warning("CBW_Battle.PriorityFunction['"..name.."'] is already defined!")
+	return nil end
+	if not(func) then //Invalid function argument
+		B.Warning("Invalid argument #2 provided for CBW_Battle.AddPriorityFunction()")
+	return nil end
+	if not(type(func) == "function") then //argument type is not a function
+		B.Warning("Argument #2 for CBW_Battle.AddPriorityFunction() must be a function!")
+	return nil end
+	PFunc[name] = func
+	return name
+end
+
+local Add = B.AddPriorityFunction
+
+Add("tails_fly",B.SPriority_Fly)
+Add("knuckles_glide",B.SPriority_Glide)
+Add("amy_twinspin",B.SPriority_TwinSpin)
+Add("amy_melee",B.SPriority_Melee)
+Add("fang_tailbounce",B.SPriority_TailBounce)
+Add("can_damage",B.SPriority_CanDamage)
+//Add("stomp_damage",B.SPriority_StompDamage)
