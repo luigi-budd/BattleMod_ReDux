0497e87de3aeb6f0507e074990eb85952739dcb9
krabs <>
2021-03-15 14:13:28 -0400

    knux fix

--- ZBa_BattleMod_CE/Lua/3-Functions/3-Player/Lib_Knuckles_Collide.lua
+++ ZBa_BattleMod_CE/Lua/3-Functions/3-Player/Lib_Knuckles_Collide.lua
@@ -1,5 +1,5 @@
 local B = CBW_Battle
-B.Knuckles_Collide = function(n1,n2,plr,mo,atk,def,weight,hurt,pain,angle,thrust,thrust2,collisiontype)
+B.Knuckles_Collide = function(n1,n2,plr,mo,atk,def,weight,hurt,pain,ground,angle,thrust,thrust2,collisiontype)
 	if plr[n1] and mo[n1].health and not(pain[n1])
 		and (plr[n1].pflags&PF_GLIDING or plr[n1].climbing 
 			or (plr[n1].charability == CA_GLIDEANDCLIMB) and collisiontype > 1 and P_IsObjectOnGround(mo[n1]) and not(plr[n1].pflags&PF_SPINNING)
