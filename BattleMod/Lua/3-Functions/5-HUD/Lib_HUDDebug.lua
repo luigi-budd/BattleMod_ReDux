b56f0c0f2c9cb5d9d74ae6cf922ae9853af213ad
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2024-06-12 11:15:22 -0400

    mega battledebug cleanup, especially battledebug 8
    now it fits in my tiny ahh resolution

--- BattleMod/Lua/3-Functions/5-HUD/Lib_HUDDebug.lua
+++ BattleMod/Lua/3-Functions/5-HUD/Lib_HUDDebug.lua
@@ -26,18 +26,32 @@ B.DebugHUD = function(v, player, cam)
 	if not(debug) then return end
 	local xoffset = 320
 	local yoffset = 14
+	local left_xoffset = 0
+	local left_yoffset = 0
 	local flags = V_HUDTRANS|V_SNAPTOTOP|V_SNAPTORIGHT|V_PERPLAYER
+	local left_flags = V_HUDTRANS|V_SNAPTOTOP|V_SNAPTOLEFT|V_PERPLAYER
 	local align = "small-right"
+	local left_align = "small"
 	local nextline = 4
+	local boolstring = function(string2)
+		if type(string2) == "boolean" then
+			string2 = string2 and "\x83"+"Y" or "\x85"+"N"
+		elseif tostring(string2) == "nil" then
+			string2 = "?"
+		end
+		return string2
+	end
 	--Double the scale for smaller screens (illegible otherwise)
 	if v.height() < 400 then 
 		align = "right"
+		left_align = nil
 		nextline = 8
 	end
 	local addspace = function()
 		yoffset = $+nextline
 	end
 	local addline = function(string,string2)
+		string2 = boolstring(string2)
 		string = "\x86"+tostring($)+": \x80"+tostring(string2)
 		v.drawString(xoffset,yoffset,string,flags,align)
 		yoffset = $+nextline
@@ -53,6 +67,26 @@ B.DebugHUD = function(v, player, cam)
 		v.drawString(xoffset,yoffset,string,flags,align)
 		yoffset = $+nextline
 	end
+	local left_addspace = function()
+		left_yoffset = $+nextline
+	end
+	local left_addline = function(string,string2)
+		string2 = boolstring(string2)
+		string = "\x86"+tostring($)+": \x80"+tostring(string2)
+		v.drawString(left_xoffset,left_yoffset,string,left_flags,left_align)
+		left_yoffset = $+nextline
+	end
+	local left_addheader = function(string)
+		left_yoffset = $+nextline
+		string = "\x82"+string
+		v.drawString(left_xoffset,left_yoffset,string,left_flags,left_align)
+		left_yoffset = $+nextline
+	end
+	local left_subheader = function(string)
+		string = "\x88"+string
+		v.drawString(left_xoffset,left_yoffset,string,left_flags,left_align)
+		left_yoffset = $+nextline
+	end
 	--****
 	--Execute drawing
 	--****
@@ -63,7 +97,7 @@ B.DebugHUD = function(v, player, cam)
 	end
 	--Gametypes
 	if debug&DF_GAMETYPE then
-			addheader("Gametype")
+			addheader("GAMETYPE")
 			addline("RedScore",B.RedScore)
 			addline("BlueScore",B.BlueScore)
 			addline("Pinch",B.Pinch)
@@ -74,7 +108,7 @@ B.DebugHUD = function(v, player, cam)
 			addline("Timeout",B.Timeout)
 			
 		if B.ArenaGametype() then
-			subheader("Arena")
+			subheader("ARENA")
 			addline("Fighters",#A.Fighters)
 			addline("RedFighters",#A.RedFighters)
 			addline("BlueFighters",#A.BlueFighters)
@@ -87,7 +121,7 @@ B.DebugHUD = function(v, player, cam)
 		end
 		
 		if B.CPGametype() then
-			subheader("Control Point")
+			subheader("CONTROL POINT")
 			addline("IDs",#CP.ID)
 			addline("Num",CP.Num)
 			addline("Meter",CP.Meter)
@@ -97,7 +131,7 @@ B.DebugHUD = function(v, player, cam)
 		end
 		
 		if B.DiamondGametype() then
-			subheader("Ruby")
+			subheader("RUBY")
 			addline("Spawned",(D.ID ~= nil and D.ID.valid))
 			if(D.ID and D.ID.valid and D.ID.target and D.ID.target.player) then
 				addline("Holder",(D.ID.target.player.name))
@@ -111,9 +145,9 @@ B.DebugHUD = function(v, player, cam)
 	--Items
 	
 	if debug&DF_ITEM then
-		addheader("Items")
+		addheader("ITEMS")
 		addline("Global Spawns",#I.Spawns)
-		addline("Global Timer",I.SpawnTimer/TICRATE.."/"..(4-CV.ItemRate.value)*I.GlobalRate/2)
+		addline("Global Timer",I.SpawnTimer/TICRATE.."\x86/\x80"..(4-CV.ItemRate.value)*I.GlobalRate/2)
 		addline("GlobalChance Entries",#I.GlobalChance)
 		addline("Global Item Rate",I.GlobalRate)
 		addline("Local Item Rate",I.LocalRate)
@@ -123,72 +157,89 @@ B.DebugHUD = function(v, player, cam)
 	--Player
 	if debug&DF_PLAYER then
 		if player and player.valid then
-			addheader("Player")
+			addheader("PLAYER")
 			addline("SkinVars",player.skinvars)
 			addline("SkinVars.flags",B.GetSkinVarsFlags(player))
-			addline("Rank",player.rank)
 			addline("PreserveScore",player.preservescore)
 			addline("Exhaust",player.exhaustmeter*100/FRACUNIT.."%")
 			addline("LedgeExhaust",player.ledgemeter*100/FRACUNIT.."%")
-			addline("Revenge",player.revenge)
-			addline("LifeShards",player.lifeshards)
-			addline("IsEggRobo",player.iseggrobo)
-			addline("IsJettySyn",player.isjettysyn)
--- 			addline("Carry_ID",player.carry_id)
--- 			addline("Carried_Time",player.carried_time)
+			addline("Tumble",player.tumble)
+			--addline("LandLag",player.landlag)
+			left_subheader("ACTION")
+			left_addline("Allowed",player.actionallowed)
+			left_addline("Rings",player.actionrings)
+			--left_addline("Debt",player.actiondebt)
+			left_addline("State",player.actionstate)
+			--left_addline("Super",player.actionsuper)
+			left_addline("Time",player.actiontime)
+			left_addline("Cooldown",player.actioncooldown)
+			left_subheader("PRIORITY")
+			left_addline("Sfunc",player.battle_sfunc)
+			left_addline("Atk/Def",player.battle_atk.."\x86/\x80"..player.battle_def)
+			left_addline("SAtk/SDef",player.battle_satk.."\x86/\x80"..player.battle_sdef)
+			left_addline("Text",player.battle_hurttxt)
+			left_subheader("GUARD / DODGE")
+			left_addline("CanGuard",player.canguard)
+			left_addline("Guard",player.guard)
+			left_addline("GuardTics",player.guardtics)
+			left_addline("Dodge",player.safedodge)
+			left_addline("Intangible",player.intangible)
+			left_addline("Stale",player.dodgecooldown)
+			subheader("SPECTATOR")
 			addline("BattleSpawning",player.battlespawning)
 			addline("SpectatorTime",player.spectatortime)
 			addline("DeadTimer",player.deadtimer)
-			addline("Intangible",player.intangible)
-			addline("AirGun",player.airgun)
-			addline("Tumble",player.tumble)
-			subheader("Action")
-			addline("Allowed",player.actionallowed)
-			addline("super",player.actionsuper)
-			addline("state",player.actionstate)
-			addline("time",player.actiontime)
-			addline("rings",player.actionrings)
-			--addline("debt",player.actiondebt)
-			addline("cooldown",player.actioncooldown)
-			subheader("Battle")
-			addline("sfunc",player.battle_sfunc)
-			addline("atk",player.battle_atk)
-			addline("def",player.battle_def)
-			addline("satk",player.battle_satk)
-			addline("sdef",player.battle_sdef)
-			addline("text",player.battle_hurttxt)
-			subheader("Guard")
-			addline("CanGuard",player.canguard)
-			addline("guard",player.guard)
-			addline("guardtics",player.guardtics)
+			if B.ArenaGametype() then
+				subheader("ARENA STATS")
+				addline("Rank",player.rank)
+				addline("Revenge",player.revenge)
+				addline("LifeShards",player.lifeshards)
+				addline("IsEggRobo",player.iseggrobo)
+				addline("IsJettySyn",player.isjettysyn)
+			end
 		end
 		if player and player.valid and player.mo and player.mo.valid then
-			subheader("General")
-			addline("ID",player.mo)
-			addline("target",player.mo.target)
-			addline("tracer",player.mo.tracer)
+			local flags = S[player.mo.skin] and S[player.mo.skin].flags or S[-1].flags
+			if (flags & SKINVARS_ROSY) then
+				left_subheader("HAMMER")
+				left_addline("Melee state",player.melee_state)
+				left_addline("Melee charge",player.melee_charge*100/FRACUNIT.."%")
+			end
+			if (flags & SKINVARS_GUNSLINGER) then
+				left_subheader("POPGUN")
+				left_addline("AirGun",player.airgun)
+				left_addline("Weapon delay",player.weapondelay)
+			end
+			if (player.charability2 == CA2_SPINDASH) then
+				left_subheader("SPINDASH")
+				left_addline("Min/Max speed",player.mindash/FRACUNIT.."\x86/\x80"..player.maxdash/FRACUNIT)
+				left_addline("Charge",player.dashspeed*100/player.maxdash.."%")
+			end
+			subheader("GENERAL")
+			addline("ID",tostring(player.mo):gsub("userdata: ",""))
+			addline("Target",tostring(player.mo.target):gsub("userdata: ",""))
+			addline("Tracer",tostring(player.mo.tracer):gsub("userdata: ",""))
 			addline("Carry",player.powers[pw_carry])
 			addline("Flashing",player.powers[pw_flashing])
 			addline("NoControl",player.powers[pw_nocontrol])
 			addline("JumpFactor",player.jumpfactor)
 			addline("ThrustFactor",player.thrustfactor)
-			addline("Lock Aim",player.lockaim)
-			addline("Lock Move",player.lockmove)
+			addline("Lock Aim/Move",boolstring(player.lockaim).."\x86/\x80"..boolstring(player.lockmove))
 			addline("Pushed Credit",player.pushed_creditplr)
 		end
 	end
 	--Collision
 	if debug&DF_COLLISION then
-		addheader("Collision")
+		addheader("COLLISION")
 		if player and player.valid and player.mo and player.mo.valid then
-			subheader("player.mo")
-			addline("pushed_last",player.mo.pushed_last)
-			addline("pushtics",player.mo.pushtics)
-			addline("weight",player.mo.weight*100/FRACUNIT.."%")
+			subheader("PLAYER.MO")
+			addline("Pushed_last",player.mo.pushed_last)
+			addline("Pushtics",player.mo.pushtics)
+			addline("Weight",player.mo.weight*100/FRACUNIT.."%")
 		end
 		local T = B.TrainingDummy
 		if T and T.valid then
-			subheader("Training Dummy")
+			subheader("TRAINING DUMMY")
 			addline("Hits",B.HitCounter)
 			addline("Fuse",T.fuse)
 			addline("Pain",T.pain)
