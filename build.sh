3e8ec9928b7512b1bd12e08dc0629529be75af2b
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2024-06-16 02:38:04 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 # therefore, building starts from this script.
 export PK3_FLAGS_DEF='ZBa'
 export PK3_NAME_DEF='BattleMod'
-export PK3_VERSION_DEF=9
-export PK3_SUBVERSION_DEF=4
+export PK3_VERSION_DEF=10
+export PK3_SUBVERSION_DEF=0
 
 export FOLDER_NAME_DEF='BattleMod'
 export PK3_EXCLUDE_DEF='.git\|./*.dbs\|./*.backup*' # grep-style exclude
