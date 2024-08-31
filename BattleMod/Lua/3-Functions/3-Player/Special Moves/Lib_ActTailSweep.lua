ec8357ae58a637d459f3788c1b7b517bbe6c6d43
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2024-08-31 13:12:26 -0400

    whoops

--- BattleMod/Lua/3-Functions/3-Player/Special Moves/Lib_ActTailSweep.lua
+++ BattleMod/Lua/3-Functions/3-Player/Special Moves/Lib_ActTailSweep.lua
@@ -186,7 +186,7 @@ B.CarryStun = function(otherplayer, strugglerings, strugglestun, noshake, nostun
 		otherplayer.powers[pw_nocontrol] = max($,strugglestun)
 		otherplayer.canstunbreak = 0
 		otherplayer.mo.hitstun_tics = otherplayer.powers[pw_nocontrol]
-		if noshake then
+		if not noshake then
 			local shake = P_SpawnMobjFromMobj(otherplayer.mo, 0, 0, 0, MT_THOK)
 			shake.state = S_SHAKE
 			otherplayer.shakemobj = shake
