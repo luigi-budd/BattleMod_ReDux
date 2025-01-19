f2c9461db4be758106042416d93262aa90f4898a
gm3k4g <thingstuffet@gmail.com>
2025-01-19 02:51:16 +0200

    avoid catastrophic failure

--- BattleMod/Lua/3-Functions/2-Game/Ruby/Lib_ModeRuby.lua
+++ BattleMod/Lua/3-Functions/2-Game/Ruby/Lib_ModeRuby.lua
@@ -335,8 +335,9 @@ R.Thinker = function(mo)
 	local on_rflagbase = (GetSecSpecial(sector.special, 4) == 3) or (sector.specialflags&SSF_REDTEAMBASE)
 	local on_bflagbase = (GetSecSpecial(sector.special, 4) == 4) or (sector.specialflags&SSF_BLUETEAMBASE)
 	local on_return_sector = P_MobjTouchingSectorSpecialFlag(mo, SSF_RETURNFLAG) -- rev: i don't know if this even works..
+	local plr_has_ruby = mo.target and mo.target.valid
 
-	if ruby_in_goop or (on_rflagbase or on_bflagbase or on_return_sector) then
+	if 	not plr_has_ruby and (ruby_in_goop or (on_rflagbase or on_bflagbase or on_return_sector)) then
 		print("fell into removal sector")
 		if (mo.target and mo.target.valid) then
 			B.PrintGameFeed(player, " dropped the "+rubytext+".")
