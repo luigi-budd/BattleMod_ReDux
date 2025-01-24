4f454e4b50b86f09301788ea6bcd4fd2ea2856b0
Mari0shi <>
2025-01-24 08:25:25 -0500

    mfw

--- BattleMod/Lua/1-Init/Init_Battle.lua
+++ BattleMod/Lua/1-Init/Init_Battle.lua
@@ -47,7 +47,7 @@ rawset(_G,"SKINVARS_GUNSLINGER",	1<<2)
 rawset(_G,"SKINVARS_ROSY",			1<<3)
 rawset(_G,"SKINVARS_GLIDESTRAFE",	1<<4)
 rawset(_G,"SKINVARS_GLIDESOUND",	1<<5)
-rawset(_G,"SKINVARS_DASHMODENERF",	1<<5)
+rawset(_G,"SKINVARS_DASHMODENERF",	1<<6)
 
 -- I hate srb2
 rawset(_G,"GTR_FIXGAMESET",	1<<0)
