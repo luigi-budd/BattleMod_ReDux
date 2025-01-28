d2cace227c96137a76eb39aea01afa19b8c21916
Mari0shi <>
2025-01-28 08:56:27 -0500

    Basic chaos ring hud

--- BattleMod/Lua/3-Functions/5-HUD/Lib_HUDTeammates.lua
+++ BattleMod/Lua/3-Functions/5-HUD/Lib_HUDTeammates.lua
@@ -71,6 +71,22 @@ B.TeammateHUD = function(v, player)
 				v.drawScaled(x + offset + offset2, y + stock_yoff, scale2, v.cachePatch("HUD_STOK"), flags | trans, playercol)
 			end
 		end
+
+		if gametype == GT_BANK then
+			
+			for t = 1, 2 do
+				local val = 1
+				local flip = (t==1 and val) or -val
+				for i = 1, 6 do
+					local num = i
+					local chaosring = B.ChaosRing.LiveTable[num]
+					if not((chaosring.captured and (chaosring.ctfteam == t) and (((chaosring.fuse) and ((leveltime/6)%2)==1) or not(chaosring.fuse))) or ((chaosring.target and chaosring.target.valid and chaosring.target.player and (chaosring.target.player.ctfteam == t) and chaosring.target.player.gotcrystal_time) and (leveltime%2)==1)) then continue end
+					local patch = v.cachePatch("CHRING"..num)
+					v.drawScaled((x-(FRACUNIT*3)+(flip*((FRACUNIT/3)*(28)))*(i+2)), (y+(FRACUNIT*10)), FRACUNIT/2, patch, flags|V_HUDTRANS)
+				end
+			end
+		end
+
 	end
 
 	--// rev: If autobalance is happening, let's tell everyone about it
