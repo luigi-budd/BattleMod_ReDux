eed860ae1ee3b0eb681ec1f45e59b91c74d20a36
Mari0shi <>
2025-02-03 15:10:54 -0500

    Account for gotmaxrings in actioncontrol

--- BattleMod/Lua/3-Functions/3-Player/Lib_ActionControl.lua
+++ BattleMod/Lua/3-Functions/3-Player/Lib_ActionControl.lua
@@ -86,6 +86,7 @@ B.CanDoAction=function(player)
 	if B.TagGametype() and not(player.pflags&PF_TAGIT or player.battletagIT) then return false end
 	if player.gotflag then return false end
 	if player.gotcrystal then return false end
+	if player.gotmaxrings then return false end
 	if player.isjettysyn then return false end
 	if player.powers[pw_nocontrol] then return false end
 	if player.powers[pw_carry] then return false end
