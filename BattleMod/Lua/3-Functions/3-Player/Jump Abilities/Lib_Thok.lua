1f93bebdaa42ce9995b854e507efd92b6b95f48d
Lumyni <24883013+felixLumyni@users.noreply.github.com>
2025-02-11 02:40:14 -0400

    Thok: Don't add momz into it, change visual effect

--- BattleMod/Lua/3-Functions/3-Player/Jump Abilities/Lib_Thok.lua
+++ BattleMod/Lua/3-Functions/3-Player/Jump Abilities/Lib_Thok.lua
@@ -1,35 +1,39 @@
 local B = CBW_Battle
 
+local spawncircle = function(mo)
+    local circle = P_SpawnMobjFromMobj(mo, 0, 0, P_MobjFlip(mo)*(mo.scale * 24), MT_THOK)
+    circle.sprite = SPR_STAB
+    circle.frame = TR_TRANS50|FF_PAPERSPRITE|A
+    circle.angle = mo.angle + ANGLE_90
+    circle.fuse = 7
+    circle.scale = mo.scale / 3
+    circle.colorized = true
+    circle.color = mo.color
+    return circle
+end
+
 B.UncappedThok = function(player, mo)
     if player.pflags&PF_THOKKED then return true end
+    local xyspd = FixedHypot(mo.momx, mo.momy)
     local actionspd = FixedMul(mo.scale, player.actionspd) / B.WaterFactor(mo)
-    
-    if (player.speed > player.normalspeed*3) then
-        P_InstaThrust(mo, mo.angle, max(actionspd,player.speed-mo.momz))
-    else
-        P_InstaThrust(mo, mo.angle, max(actionspd,player.speed))
-    end
-    
-    S_StartSound(mo, sfx_thok)
-    if player.speed > (actionspd+FRACUNIT) then
-        local circle = P_SpawnMobjFromMobj(mo, 0, 0, P_MobjFlip(mo)*(mo.scale * 24), MT_THOK)
-        circle.sprite = SPR_STAB
-        circle.frame = TR_TRANS50|FF_PAPERSPRITE|_G["A"]
-        circle.angle = mo.angle + ANGLE_90
-        circle.fuse = 7
-        circle.scale = mo.scale / 3
-        circle.destscale = 10*mo.scale
-        circle.colorized = true
-        circle.color = mo.color
-        circle.momx = -mo.momx / 2
-        circle.momy = -mo.momy / 2
+
+    if xyspd > actionspd then
+        S_StartSound(mo, sfx_thok)
         S_StartSound(mo, sfx_dash)
         if (player == displayplayer) then
             P_StartQuake(9*FRACUNIT, 2)
         end
+        local circle1 = spawncircle(mo)
+        circle1.fuse = $ + 3
+        circle1.destscale = circle1.scale * 2
+        circle1.scalespeed = circle1.destscale / circle1.fuse
+        local circle2 = spawncircle(mo)
+        circle2.blendmode = AST_ADD
     else
+        S_StartSound(mo, sfx_thok)
         P_SpawnThokMobj(player)
     end
+    P_InstaThrust(mo, mo.angle, max(actionspd, xyspd))
     player.drawangle = mo.angle
     
     player.pflags = $|PF_THOKKED &~ PF_SPINNING
