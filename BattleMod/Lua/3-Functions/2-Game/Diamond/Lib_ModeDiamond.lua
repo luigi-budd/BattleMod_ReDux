11bab9c18af688b14888766d74ede88d1180aad0
Mari0shi <>
2025-02-13 13:07:17 -0500

    Same for topaz

--- BattleMod/Lua/3-Functions/2-Game/Diamond/Lib_ModeDiamond.lua
+++ BattleMod/Lua/3-Functions/2-Game/Diamond/Lib_ModeDiamond.lua
@@ -227,16 +227,27 @@ local function spawn_steal_sparks(mo, player, scale, speed, height, spawn_explos
 	end
 end
 
-D.Collect = function(mo,toucher)
-	if mo.target == toucher or not(toucher.player) then return end --This toucher has already collected the item, or is not a player
-	if P_PlayerInPain(toucher.player) or toucher.player.powers[pw_flashing] then return end --Can't touch if we've recently taken damage
-	if toucher.player.tossdelay then return end --Can't collect if tossflag is on cooldown
-	local previoustarget = mo.target
-	mo.lasttouched = previoustarget or toucher
-	if CV.DiamondDisableStealing.value == 1 and previoustarget ~= nil then return end
-	if previoustarget ~= nil and previoustarget.player.powers[pw_flashing] > 0 then return end
-	--if previoustarget ~= nil and (previoustarget.player.guard == 1 or previoustarget.player.guardtics > 0) then return end
+D.Collect = function(mo,toucher,playercansteal)
+	local diamondIsValid = (mo and mo.valid)
+	local targetIsValid = diamondIsValid and (mo.target and mo.target.valid)
+	local toucherIsValid = (toucher and toucher.valid)
+	local targetIsToucher = targetIsValid and toucherIsValid and (mo.target == toucher)
+	local toucherIsPlayer = toucherIsValid and toucher.player
+	local targetIsPlayer = targetIsValid and mo.target.player
+	local toucherIsPlayerInPain = toucherIsValid and toucherIsPlayer and P_PlayerInPain(toucher.player)
+	local toucherIsFlashing = toucherIsValid and toucherIsPlayer and toucher.player.powers[pw_flashing]
 
+	local teammatepass = (G_GametypeHasTeams() and (targetIsValid and toucherIsValid) and not(targetIsToucher) and (toucherIsPlayer and targetIsPlayer) and (toucher.player.ctfteam == mo.target.player.ctfteam) and (mo.target.player.cmd.buttons & BT_TOSSFLAG))
+
+	
+	if targetIsValid and toucherIsValid then
+		if toucherIsPlayer and targetIsPlayer then
+			if (toucherIsPlayerInPain) or (toucherIsFlashing) then return true end
+			if not(playercansteal or teammatepass) then return true end
+		end
+	end
+
+	local previoustarget = mo.target
 	mo.target = toucher
 	free(mo)
 	mo.idle = nil
@@ -386,6 +397,16 @@ D.Thinker = function(mo)
 	end
 	mo.frame = $|FF_FULLBRIGHT
 	mo.angle = $+rotatespd
+
+	if mo and mo.valid and G_GametypeHasTeams() then
+		if mo.target and mo.target.valid and mo.target.player and mo.target.player.gotcrystal then
+			local btns = mo.target.player.cmd.buttons
+			if (btns&BT_TOSSFLAG) and mo.target.player.ctfteam == displayplayer.ctfteam then
+				P_SpawnLockOn(displayplayer, mo.target, S_LOCKON1)
+			end
+		end
+	end
+
 	for player in players.iterate
 		if not player.mo then continue end
 		if D.Diamond and D.Diamond.valid and not(player.mo.btagpointer) then
@@ -396,7 +417,7 @@ D.Thinker = function(mo)
 			end
 		end
 		if player.mo == mo.target then
-			if player.cmd.buttons&BT_TOSSFLAG and not(player.tossdelay) then
+			/*if player.cmd.buttons&BT_TOSSFLAG and not(player.tossdelay) then
 				free(mo)
 				mo.target = nil
 				player.actioncooldown = TICRATE
@@ -406,12 +427,12 @@ D.Thinker = function(mo)
 				P_InstaThrust(mo,player.mo.angle,FRACUNIT*5)
 				P_SetObjectMomZ(mo,FRACUNIT*10)
 				player.tossdelay = TICRATE*2
-			else
+			else*/
 				player.gotcrystal = true
 				--points(player)
 				--player.gotcrystal_time = $ + 1
 				-- Add capture point code here..?
-			end
+			--end
 		else
 			player.gotcrystal = false
 			player.gotcrystal_time = 0
@@ -421,7 +442,7 @@ D.Thinker = function(mo)
 	if mo.flags&MF_SPECIAL and mo.target and mo.target.valid 
 	and mo.target.pushed_last and mo.target.pushed_last.valid
 	and CV.DiamondDisableStealing.value == 0 then
-		D.Collect(mo,mo.target.pushed_last)
+		D.Collect(mo,mo.target.pushed_last,true)
 	end
 	
 	--Owner has taken damage or has gone missing
